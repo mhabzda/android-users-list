0c81f8ff81f3635b39ee4d4355bc1a258e495e5c
mhabzda <marcin.habzda1@gmail.com>
2020-05-15 17:26:02 +0200

    Implement tests for ListPresenter

---
+++ app/src/test/java/com/users/ListPresenterTests.kt
@@ -0,0 +1,71 @@
+package com.users
+
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.users.list.model.domain.UserEntity
+import com.users.list.model.domain.UserRepository
+import com.users.list.ui.ListContract
+import com.users.list.ui.ListPresenter
+import com.users.list.ui.displayable.UserDisplayable
+import com.users.list.ui.schedulers.SchedulerProvider
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.TestScheduler
+import org.junit.Test
+
+class ListPresenterTests {
+  private val testScheduler = TestScheduler()
+  private val schedulerProvider = object : SchedulerProvider {
+    override fun io(): Scheduler {
+      return testScheduler
+    }
+
+    override fun ui(): Scheduler {
+      return testScheduler
+    }
+  }
+  private val view: ListContract.View = mock()
+
+  @Test
+  fun `given users available when fetch users then display users list`() {
+    val presenter = createPresenter(userRepository = mock {
+      on { retrieveUsers() } doReturn Observable.just(listOf(UserEntity("a", "b")))
+    })
+
+    presenter.fetchUsers()
+    testScheduler.triggerActions()
+
+    verify(view).displayUserList(listOf(UserDisplayable("a", "b", "")))
+  }
+
+  @Test
+  fun `given users unavailable when fetch users then log error`() {
+
+  }
+
+  @Test
+  fun `given repositories available when fetch users then update list item`() {
+
+  }
+
+  @Test
+  fun `given repositories unavailable when fetch users then log error`() {
+
+  }
+
+  // all logic inside presenter we can be easily tested. I'm not doing that because I don't have enough time.
+  // some operations like filtering or mapping can be even extracted
+  // to separate classes (like mappers) and tested separately
+
+  // Additionally, I should test logic inside CompositeUserRepository to make sure merge logic is working correctly.
+  // But I have no time for that.
+
+  private fun createPresenter(userRepository: UserRepository = mock()): ListPresenter {
+    return ListPresenter(
+      userRepository = userRepository,
+      schedulerProvider = schedulerProvider,
+      view = view
+    )
+  }
+}
